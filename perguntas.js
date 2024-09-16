39976ab525d0b140ff075007ac7db100d0d83649
adriane08 <139055906+adriane08@users.noreply.github.com>
2024-09-16 16:41:32 -0300

    ...........

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Conceito',
+    'Pergunta 1',
     'O que é o movimento?',
     'É o conjunto de movimentos sociais que atuam na luta contra o racismo, a discriminação e segregação racial, defendendo a igualdade social e a melhoria das condições de vida do povo negro'
 )
 
 criaCartao(
-    'Objetivos',
+    'Pergunta 2',
     'Cite alguns objetivos do movimento.',
-    'Combate ao racismo estrutural, desigualdade social. Igualdade nas oportunidades de trabalho, de remuneração. Fim da violência policial, valorização da história e cultura afro-brasileira.'
+    'Combate ao racismo estrutural, desigualdade social; Igualdade nas oportunidades de trabalho, de remuneração; Fim da violência policial; valorização da história e cultura afro-brasileira.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma fun',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Pergunta 3',
+    'Cite alguma data, simbulo ou marco do movimento.',
+    '20 de novembro; Punho fechado'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Pergunta 4',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
